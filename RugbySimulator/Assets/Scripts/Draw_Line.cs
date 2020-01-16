aab2adef2e89e6c857cd787c664285820272a155
TyraIchiro <51357734+TyraIchiro@users.noreply.github.com>
2020-01-16 20:28:06 +0900

    fix

--- RugbySimulator/Assets/Scripts/Draw_Line.cs
+++ RugbySimulator/Assets/Scripts/Draw_Line.cs
@@ -1,31 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using Microsoft.MixedReality.Toolkit.UI;
-using Microsoft.MixedReality.Toolkit.Input;
 
-public class Draw_Line : MonoBehaviour, IMixedRealityPointerHandler
+public class Draw_Line : MonoBehaviour
 {
     bool isalive = false;
-    bool tapalive = false;
-    GameObject ally;
-    ManipulationHandler a_hnd;
+    GameObject field;
 
     // Start is called before the first frame update
     void Start()
     {
-        ally = GameObject.Find("Ally_09");
-        a_hnd = ally.GetComponent<ManipulationHandler>();
+        field = GameObject.Find("AllGround");
+        //field.GetComponent<ManipulationHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(tapalive)
-        {
-            
-        }
+        
     }
 
     public void Draw_Stop()
@@ -33,38 +25,10 @@ public class Draw_Line : MonoBehaviour, IMixedRealityPointerHandler
         if (isalive)
         {
             isalive = false;
-            a_hnd.enabled = true;
         }
         else
         {
             isalive = true;
-            a_hnd.enabled = false;
-        }
-    }
-
-    public void OnPointerClicked(MixedRealityPointerEventData eventData)
-    {
-        
-    }
-
-    public void OnPointerDown(MixedRealityPointerEventData eventData)
-    {
-        if (isalive)
-        {
-            tapalive = true;
-        }
-    }
-
-    public void OnPointerDragged(MixedRealityPointerEventData eventData)
-    {
-
-    }
-
-    public void OnPointerUp(MixedRealityPointerEventData eventData)
-    {
-        if (isalive)
-        {
-            tapalive = false;
         }
     }
 }
---
+++ RugbySimulator/MSBuild/Projects/buildall.bat
@@ -0,0 +1,92 @@
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.TextMeshPro.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.Analytics.DataPrivacy.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/UnityEngine.Advertisements.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/UnityEngine.Monetization.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.XR.WindowsMR.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Async.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.TextMeshPro.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.PackageManagerUI.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.MSBuild.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Editor.Utilities.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.CollabProxy.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Unity.XR.WindowsMR.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Editor.ClassExtensions.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.SpatialAwarenessSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Gltf.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.SceneSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputSimulation.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.DiagnosticsSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.InspectorFields.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Providers.WindowsMixedReality.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.Gltf.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Providers.WindowsVoiceInput.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.TeleportSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.CameraSystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.BoundarySystem.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Gltf.Importers.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Editor.BuildAndDeploy.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputSystem.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.Gltf.Inspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Providers.OpenVR.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputAnimation.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Editor.Inspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Editor.ServiceInspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.InspectorFields.Inspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Tools.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Providers.ObjectMeshObserver.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputSimulation.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Services.InputAnimation.Editor.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.SDK.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Examples.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.SDK.Inspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.StandardShader.Inspectors.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Microsoft.MixedReality.Toolkit.Demos.UX.Interactables.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+dotnet build D:/Programming/Unity/RugbySimulator/RugbySimulator/MSBuild/Projects/Assembly-CSharp.csproj -p:"UnityConfiguration=%1;UnityPlatform=%2;MSBuildExtensionsPath=%~3" --no-dependencies
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
